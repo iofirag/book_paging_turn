1b33cc3f2a4dc7ea80fb3856bb7a8b6a37d88ab9
ofir agahi <iofirag@gmail.com>
2014-09-09 02:48:59 +0300

    drag & drop and button

--- includes2/js/index.js
+++ includes2/js/index.js
@@ -1,60 +1,75 @@
+var width = $( window ).width()*0.70;
+var height = $( window ).height()*0.80;
 jQuery(document).ready(function($) {
 	$("#flipbook").turn({
-	    width: 400,
-	    height: 300,
+	    width: width,
+	    height: height,
 	    autoCenter: true
 	});
-	
-	var holder = document.getElementById('holder'),
-			    state = document.getElementById('status');
-			
-			if (typeof window.FileReader === 'undefined') {
-			  state.className = 'fail';
-			} else {
-			  state.className = 'success';
-			  state.innerHTML = 'File API & FileReader available';
-			}
-			 
-			holder.ondragover = function () { this.className = 'hover'; return false; };
-			holder.ondragend = function () { this.className = ''; return false; };
-			holder.ondrop = function (e) {
-			  this.className = '';
-			  e.preventDefault();
-			
-			  var file = e.dataTransfer.files[0],
-			      reader = new FileReader();
-			  reader.onload = function (event) {
-			    console.log(event.target);
-			    holder.style.background = 'url(' + event.target.result + ') no-repeat center';
-			  };
-			  console.log(file);
-			  reader.readAsDataURL(file);
-			
-			  return false;
-			};
+	dragAndDrop_init();
 });
+
+function dragAndDrop_init() {
+	$(".holder").css("height", (height-30)+"px");
+	var holder = $(".holder"), state = $(".status");
+	// length of 'holder' & 'state' are the same
+	for (i=0; i<holder.length; i++){
+		if (!holder[i]) 
+			i=0;
+			
+		if ( typeof window.FileReader === 'undefined') {
+			state[i].id = 'fail';
+		} else {
+			state[i].id = 'success';
+			//state[i].innerHTML = 'File API & FileReader available';
+		}
+	
+		holder[i].ondragover = function() { 
+			this.id ='hover';
+			return false;
+		};
+		holder[i].ondragend = function() { debugger;
+			this.id = '';
+			return false;
+		};
+		holder[i].ondrop = function(e) { 
+			this.id = '';
+			this.innerHTML = "";
+			e.preventDefault();
+			
+			var file = e.dataTransfer.files[0], reader = new FileReader();
+			reader.onload = function(event) {
+				console.log(event.target);
+				// 'e.target' is equal to 'holder[i]'
+				e.target.style.background = 'url(' + event.target.result + ') no-repeat center';
+			};
+			console.log(file);
+			reader.readAsDataURL(file);
+	
+			return false;
+		};
+	}
+}
+
 
 function getNumberOf_lastPage(){
 	return $("#flipbook").turn("pages");
 }
 
 function addPage(){	
+	debugger;
 	// Delete the last 3 pages
 		removePage("last");
 		removePage("last");
 		removePage("last");
 		
-		// Add normal page
-		element = $("<div />").html("Page "+(getNumberOf_lastPage()-1) );
-		$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
+		addPageElement();
 		
 		odd = getNumberOf_lastPage() % 2;
 		if (odd) {
 			console.log('odd');
 		} else {
-			// Add more normal page
-			element = $("<div />").html("Page "+(getNumberOf_lastPage()-1) );
-			$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
+			addPageElement();
 			console.log('even');
 		}
 
@@ -63,12 +78,23 @@ function addPage(){
 		$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
 
 		// Add the 2 last hard pages
-		element = $("<div class='hard' />");
+		element = $("<section class='hard' />");
 		$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
-		element = $("<div class='hard' />");
+		element = $("<section class='hard' />");
 		$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
 
+		dragAndDrop_init();
+		
 		console.log('add complete');
+}
+
+function addPageElement(){
+	// Add more normal page
+	element = $("<article />").html("<section>\
+										<section class='holder'></section>\
+										<p class='status'></p>\
+									</section>");
+	$("#flipbook").turn("addPage", element, getNumberOf_lastPage() + 1);
 }
 
 function removePage(number){
@@ -78,3 +104,5 @@ function removePage(number){
 		$("#flipbook").turn("removePage", number);
 	}
 }
+
+
